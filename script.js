c2e4f6a06ca7aee1ec390f988af7ecb308dee65a
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-11-11 11:41:49 +0300

    summary
    i famished the project but i plan on coming back to it after i learn the grid display

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const btn = document.querySelector('.btn-change-grid');
+
+btn.addEventListener('click', () => {
+    let newGrid = window.prompt('how many?');
+    if (newGrid <= 0 || newGrid > 100) {
+        alert('must be more the 0 and less then 101 ')
+    } else {
+        // to delete the grid before creating a new one
+        container.innerHTML = '';
+        createGrid(newGrid);
+    }
+})
+
+createGrid(16)// default grid
+
+function createGrid(num) {
+    let i = 1;
+    let j = 1;
+    while (i <= num) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row')
+        container.appendChild(row)
+        while (j <= num) {
+            let column = document.createElement('div')
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = 'black'
+            })
+            column.setAttribute('class', 'column')
+            row.appendChild(column)
+            j++
+        }
+        j = 1;
+        i++;
+    }
+}
